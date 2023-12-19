4507d47e65a2f2515eb16c1be468c765a77af5fc
as577 <as577@WIN-10.CTI.UGAL.RO>
2023-12-19 15:45:17 +0200

    Search function laptop

--- src/main/java/interfete/laptopint.java
+++ src/main/java/interfete/laptopint.java
@@ -1,10 +1,11 @@
 package interfete;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import com.mycompany.magazinelectronice.*;
 import java.awt.*;
+import java.util.*;
+import java.util.List;
 import newpackage.*;
 
 public class laptopint extends javax.swing.JFrame {
@@ -18,6 +19,7 @@ public class laptopint extends javax.swing.JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
         initializareComboBox();
+        jButton1.setVisible(false);
         adaugaActionListenerComboBox();
     }
 
@@ -31,6 +33,7 @@ public class laptopint extends javax.swing.JFrame {
         jTextArea1 = new javax.swing.JTextArea();
         jComboBox1 = new javax.swing.JComboBox<>();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         menu = new javax.swing.JMenu();
         home = new javax.swing.JMenuItem();
@@ -62,6 +65,13 @@ public class laptopint extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("Search");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         menu.setText("Home");
 
         home.setText("Home");
@@ -88,10 +98,12 @@ public class laptopint extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(47, 47, 47)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 323, Short.MAX_VALUE)
-                    .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jComboBox1, 0, 323, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 323, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(28, 28, 28))
         );
         layout.setVerticalGroup(
@@ -101,13 +113,14 @@ public class laptopint extends javax.swing.JFrame {
                     .addComponent(back, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(titlu, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(74, 74, 74)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 513, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(74, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addGap(14, 14, 14))
         );
 
         pack();
@@ -130,7 +143,7 @@ public class laptopint extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         int selectedIndex = jComboBox1.getSelectedIndex();
 
-        JFrame popup = new JFrame("Detalii Telefon");
+        JFrame popup = new JFrame("Detalii Laptop");
         popup.setSize(1000, 900);
         popup.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
@@ -185,8 +198,8 @@ public class laptopint extends javax.swing.JFrame {
         addLabelAndTextField(panel, "Mărime baterie:", textFieldMarimeBat);
         addLabelAndTextField(panel, "Diagonala ecran:", textFieldDiagonalaEcran);    
         addLabelAndTextField(panel, "Diagonala ecran:", textFieldGreutate);    
-        JButton saveButton = new JButton("Salvează");
 
+        JButton saveButton = new JButton("Salvează");
         saveButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
               String valoareMarca = textFieldMarca.getText();
@@ -250,23 +263,181 @@ public class laptopint extends javax.swing.JFrame {
         popup.setVisible(true);  
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        JDialog dialog = new JDialog(this, "Cautare", true);
+        dialog.setLayout(new GridLayout(25, 0, 25, 10));
+        jComboBox1.setSelectedIndex(0);
+
+        JTextField textFieldMarca = new JTextField();
+        JTextField textFieldModel = new JTextField();
+        JTextField textFieldCantitate = new JTextField();
+        JTextField textFieldAnAparitie = new JTextField();
+        JTextField textFieldPret = new JTextField();
+        JTextField textFieldRezEcran = new JTextField();
+        JTextField textFieldProcesor = new JTextField();
+        JTextField textFieldTipEcran = new JTextField();
+        JTextField textFieldCuloare = new JTextField();
+        JTextField textFieldFrecventaRam = new JTextField();
+        JTextField textFieldSistemDeOperare = new JTextField();
+        JTextField textFieldTipPlacaVideo = new JTextField();
+        JTextField textFieldModelPlacaVideo = new JTextField();
+        JTextField textFieldTipPorturi = new JTextField();
+        JTextField textFieldAutonomieBat = new JTextField();
+        JTextField textFieldRezCamere = new JTextField();
+        JTextField textFieldNrPorturi = new JTextField();
+        JTextField textFieldMemorieRam = new JTextField();
+        JTextField textFieldNrCamere = new JTextField();
+        JTextField textFieldMarimeBat = new JTextField();
+        JTextField textFieldDiagonalaEcran = new JTextField();
+        JTextField textFieldGreutate = new JTextField();
+    
+        addLabelAndTextField(dialog, "Marca:", textFieldMarca);
+        addLabelAndTextField(dialog, "Model:", textFieldModel);
+        addLabelAndTextField(dialog, "Cantitate:", textFieldCantitate);
+        addLabelAndTextField(dialog, "An aparitie:", textFieldAnAparitie);
+        addLabelAndTextField(dialog, "Pret:", textFieldPret);
+        addLabelAndTextField(dialog, "Procesor:", textFieldProcesor);
+        addLabelAndTextField(dialog, "Tip ecran:", textFieldTipEcran);
+        addLabelAndTextField(dialog, "Culoare:", textFieldCuloare);
+        
+        addLabelAndTextField(dialog, "Frecventa ram:", textFieldFrecventaRam);
+        addLabelAndTextField(dialog, "Sistem de operare:", textFieldSistemDeOperare);
+        addLabelAndTextField(dialog, "Tip placa video:", textFieldTipPlacaVideo);
+        addLabelAndTextField(dialog, "Model placa video:", textFieldModelPlacaVideo);
+        addLabelAndTextField(dialog, "Tip porturi:", textFieldTipPorturi);
+        addLabelAndTextField(dialog, "Autonomie baterie:", textFieldAutonomieBat);
+        addLabelAndTextField(dialog, "Rezoluție ecran:", textFieldRezEcran);
+        addLabelAndTextField(dialog, "Rezoluție camere:", textFieldRezCamere);
+        addLabelAndTextField(dialog, "Nr porturi:", textFieldNrPorturi);
+        addLabelAndTextField(dialog, "Memorie RAM:", textFieldMemorieRam);
+        addLabelAndTextField(dialog, "Număr camere:", textFieldNrCamere);
+        addLabelAndTextField(dialog, "Mărime baterie:", textFieldMarimeBat);
+        addLabelAndTextField(dialog, "Diagonala ecran:", textFieldDiagonalaEcran);    
+        addLabelAndTextField(dialog, "Diagonala ecran:", textFieldGreutate);
+
+        StringBuilder resultBuilder = new StringBuilder();
+
+        JButton searchButton = new JButton("Cauta");
+        searchButton.addActionListener(event -> {
+            String marca = textFieldMarca.getText().trim();
+            String model = textFieldModel.getText().trim();
+            String cantitateText = textFieldCantitate.getText().trim();
+            String anAparitieText = textFieldAnAparitie.getText().trim();
+            String pretText = textFieldPret.getText().trim();
+            String procesor = textFieldProcesor.getText().trim();
+            String tipEcran = textFieldTipEcran.getText().trim();
+            String culoare = textFieldCuloare.getText().trim();
+            String frecventaRam = textFieldFrecventaRam.getText().trim();
+            String sistemDeOperare = textFieldSistemDeOperare.getText().trim();
+            String tipPlacaVideo = textFieldTipPlacaVideo.getText().trim();
+            String modelPlacaVideo = textFieldModelPlacaVideo.getText().trim();
+            String tipPorturi = textFieldTipPorturi.getText().trim();
+            String autonomieBat = textFieldAutonomieBat.getText().trim();
+            String rezEcran = textFieldRezEcran.getText().trim();
+            String rezCamere = textFieldRezCamere.getText().trim();
+            String nrPorturi = textFieldNrPorturi.getText().trim();
+            String memorieRAM = textFieldMemorieRam.getText().trim();
+            String nrCamere = textFieldNrCamere.getText().trim();
+            String marimeBat = textFieldMarimeBat.getText().trim();
+            String diagonalaEcran = textFieldDiagonalaEcran.getText().trim();
+            String greutate = textFieldGreutate.getText().trim();
+
+            List<Laptop> foundDevices = new ArrayList<>();
+            boolean emptyTextFields = false;
+            
+            if (marca.isEmpty() && model.isEmpty() 
+                && cantitateText.isEmpty() && anAparitieText.isEmpty() 
+                && pretText.isEmpty() && procesor.isEmpty()
+                && tipEcran.isEmpty() && culoare.isEmpty()
+                && frecventaRam.isEmpty() && sistemDeOperare.isEmpty()
+                && tipPlacaVideo.isEmpty() && modelPlacaVideo.isEmpty()
+                && tipPorturi.isEmpty() && autonomieBat.isEmpty()
+                && rezEcran.isEmpty() && rezCamere.isEmpty()
+                && nrPorturi.isEmpty() && memorieRAM.isEmpty()
+                && nrCamere.isEmpty() && marimeBat.isEmpty()
+                && diagonalaEcran.isEmpty() && greutate.isEmpty()) {
+                    emptyTextFields = true;
+            }
+            
+            if(emptyTextFields) {
+                foundDevices.addAll(Arrays.asList(laptopuri));
+            } else {
+                for (Laptop instance : laptopuri) {
+                    boolean match = true;
+                    
+                    if (!marca.isEmpty() && !instance.getMarca().equals(marca)
+                        || !model.isEmpty() && !instance.getModel().equals(model)
+                        || !cantitateText.isEmpty() && instance.getCantitate() != Integer.parseInt(cantitateText)
+                        || !anAparitieText.isEmpty() && instance.getAnAparitie() != Integer.parseInt(anAparitieText)
+                        || !pretText.isEmpty() && instance.getPret() != Float.parseFloat(pretText)
+                        || !procesor.isEmpty() && !instance.getProcesor().equals(procesor)
+                        || !tipEcran.isEmpty() && !instance.getTipEcran().equals(tipEcran)
+                        || !culoare.isEmpty() && !instance.getCuloare().equals(culoare)
+                        || !frecventaRam.isEmpty() && !instance.getFrecventaRam().equals(frecventaRam)
+                        || !sistemDeOperare.isEmpty() && !instance.getSistemDeOperare().equals(sistemDeOperare)
+                        || !tipPlacaVideo.isEmpty() && !instance.getTipPlacaVideo().equals(tipPlacaVideo)
+                        || !modelPlacaVideo.isEmpty() && !instance.getModelPlacaVideo().equals(modelPlacaVideo)
+                        || !tipPorturi.isEmpty() && !instance.getTipPorturi().equals(tipPorturi)
+                        || !autonomieBat.isEmpty() && !instance.getAutonomieBat().equals(autonomieBat)
+                        || !rezEcran.isEmpty() && !instance.getRezEcran().equals(rezEcran)
+                        || !rezCamere.isEmpty() && instance.getRezCamere() != Integer.parseInt(rezCamere)
+                        || !nrPorturi.isEmpty() && instance.getNrPorturi() != Integer.parseInt(nrPorturi)
+                        || !memorieRAM.isEmpty() && instance.getMemorieRam() != Integer.parseInt(memorieRAM)
+                        || !nrCamere.isEmpty() && instance.getNrCamere() != Integer.parseInt(nrCamere)
+                        || !marimeBat.isEmpty() && instance.getMarimeBat() != Integer.parseInt(marimeBat)
+                        || !diagonalaEcran.isEmpty() && instance.getDiagonalaEcran() != Float.parseFloat(diagonalaEcran)
+                        || !greutate.isEmpty() && instance.getGreutate() != Float.parseFloat(greutate)) {
+                        match = false;
+                    }
+                    
+                    if (match) foundDevices.add(instance);
+                }
+            }
+
+            if (foundDevices.isEmpty()) {
+                if(!emptyTextFields) {
+                    jTextArea1.setText("NU S-A GASIT NICIUN PRODUS");
+                    dialog.dispose();
+                }
+            } else {
+                for (Laptop laptop : foundDevices) {
+                    resultBuilder.append(laptop).append("\n\n");
+                }
+                
+                jTextArea1.setText(resultBuilder.toString());
+                dialog.dispose();
+            }
+        });
+
+        dialog.add(searchButton);
+        dialog.setSize(600, 800);
+        dialog.setLocationRelativeTo(this);
+        dialog.setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     private void addLabelAndTextField(JPanel panel, String labelText, JTextField textField) {
         JLabel label = new JLabel(labelText);
         panel.add(label);
         panel.add(textField);
-}
+    }
+    
+    private void addLabelAndTextField(JDialog panel, String labelText, JTextField textField) {
+        JLabel label = new JLabel(labelText);
+        panel.add(label);
+        panel.add(textField);
+    }
     
     private void initializareComboBox() {
-        String[] numeLap = new String[laptopuri.length];
+        String[] numeLap = new String[laptopuri.length + 1];
+        numeLap[0] = "Alege Laptop";
         
         for(int i = 0; i < laptopuri.length; i++) {
             String marca = laptopuri[i].getMarca();
             String model = laptopuri[i].getModel();
-            numeLap[i] = marca + " " + model;
+            numeLap[i + 1] = marca + " " + model;
         }
 
         jComboBox1.setModel(new DefaultComboBoxModel<>(numeLap));
-        jTextArea1.setText(laptopuri[0].toString());
     }
     
     private void adaugaActionListenerComboBox() {
@@ -274,11 +445,14 @@ public class laptopint extends javax.swing.JFrame {
             public void actionPerformed(ActionEvent e) {
                 int selectedIndex = jComboBox1.getSelectedIndex();
                 
-                if(selectedIndex >= 0) {
+                if(selectedIndex >= 1) {
                     Object obiectSelectat = laptopuri[selectedIndex];
                     String textAfisat = obiectSelectat.toString();
                     jTextArea1.setText(textAfisat);
                     jButton1.setVisible(true);
+                } else {
+                    jTextArea1.setText("");
+                    jButton1.setVisible(false);
                 }
             }
         });
@@ -300,6 +474,7 @@ public class laptopint extends javax.swing.JFrame {
     private java.awt.Button back;
     private javax.swing.JMenuItem home;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JScrollPane jScrollPane1;
